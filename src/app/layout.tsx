b838bee1868c7dfe2ccbf2689ed3d34c5ea75af8
arieen <arielleharyanto@gmail.com>
2025-03-08 14:50:16 -0800

    Testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
+import { Inter } from "next/font/google";
 import { ReactQueryClientProvider } from "@/utils/react-query";
 
 const inter = Inter({ subsets: ["latin"] });
