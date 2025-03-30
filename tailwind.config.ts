054bb81967862f289e2771495cf8e7c0013a2180
Brandon Tran <brandontran1220@gmail.com>
2025-03-29 22:56:51 -0700

    Add images and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,14 @@ module.exports = {
       fontFamily: {
         playfair: ["var(--playfair-font)"],
       },
+      colors: {
+        naama: {
+          "blue-100": "#86ADEA",
+          "blue-200": "#25459A",
+          "blue-300": "#0A2469",
+          "ivory-100": "#F7F2EB",
+        },
+      },
     },
   },
   plugins: [],
