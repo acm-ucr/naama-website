10c1c589d5dd8b2d62ddef3976b73869f93c57a4
Muhammad Sabeel <muhammadsabeel4@gmail.com>
2025-04-04 11:57:42 -0700

    completed issue

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,12 +1,22 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="flex w-3/4 items-center gap-1 bg-red-200 p-10">
-      <div className="w-1/2 bg-green-200 p-5 text-left">{props.text1}</div>
-      <div className="flex w-1/2 flex-row gap-1">
-        <div className="w-full bg-green-200 p-5 text-left">props.text2</div>
-        <div className="w-full bg-green-200 p-5 text-right">{props.text2}</div>
+    <div className="bg-naama-ivory-100 flex w-2/3 flex-col items-center gap-y-8 p-10">
+      <div className="bg-naama-blue-100 flex w-1/2 items-center justify-center p-5 text-center text-white">
+        {props.text1}
       </div>
-      {/* Hint: Add another box/div here similarly to line 4 */}
+      <div className="flex w-1/2 flex-row gap-x-10">
+        <div className="bg-naama-blue-200 flex w-full items-center justify-center rounded-full p-5 text-center text-white">
+          {props.text2}
+        </div>
+        <div className="bg-naama-blue-200 flex w-full items-center justify-center rounded-full p-5 text-white not-odd:text-center">
+          {props.text2}
+        </div>
+      </div>
+      {
+        <div className="bg-naama-blue-300 flex w-1/2 items-center justify-center p-5 text-center text-white">
+          {props.text1}
+        </div>
+      }
     </div>
   );
 };
