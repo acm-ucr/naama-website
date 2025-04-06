b3e10934d2ee82653fce4318b5bc5a404bc49387
JSimjee <jameelsimjee@gmail.com>
2025-04-06 14:28:25 -0700

    started

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,12 @@
+import { GoArrowUpLeft } from "@/react-icons";
 const Button = () => {
-  return <div>Button</div>;
+  return (
+  <div roundedButton="w-22 h-22 bg-">
+
+  </div>;
+  )
 };
+// Font: Playfair Display, 18px, 96x22px, color #25459A
+// Button is 133x40 px, radius 25px
 
 export default Button;
