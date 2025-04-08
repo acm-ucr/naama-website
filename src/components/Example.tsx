57664ceb7c2ed37583c7643c9cf77b4d1f08fee3
Matthew Martinez <matthewmartinez@ucr-secure-43-10-13-222-72.wnet.ucr.edu>
2025-04-08 15:21:07 -0700

    Finished example component, ready for review

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,12 +1,23 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="flex w-3/4 items-center gap-1 bg-red-200 p-10">
-      <div className="w-1/2 bg-green-200 p-5 text-left">{props.text1}</div>
-      <div className="flex w-1/2 flex-row gap-1">
-        <div className="w-full bg-green-200 p-5 text-left">props.text2</div>
-        <div className="w-full bg-green-200 p-5 text-right">{props.text2}</div>
+    <div className="bg-naama-ivory-100 flex w-2/3 flex-col items-center gap-12 rounded-2xl p-10">
+      <div className="flex w-full justify-center">
+        <div className="bg-naama-blue-200 flex w-2/3 justify-center p-5 text-center text-3xl">
+          {props.text1}
+        </div>
       </div>
-      {/* Hint: Add another box/div here similarly to line 4 */}
+
+      <div className="flex w-2/3 justify-center gap-x-10">
+        <div className="bg-naama-blue-100 w-full rounded-full p-5 text-center text-3xl text-white">
+          {props.text2}
+        </div>
+        <div className="bg-naama-blue-100 w-full gap-12 rounded-full p-5 text-center text-3xl text-white">
+          {props.text2}
+        </div>
+      </div>
+      <div className="bg-naama-blue-300 flex w-2/3 justify-center gap-12 p-5 text-center text-3xl text-white">
+        {props.text3}
+      </div>
     </div>
   );
 };
