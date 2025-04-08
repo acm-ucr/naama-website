eb703ce8770a7c5a012d792c37e5e9bac04bf94f
JSimjee <jameelsimjee@gmail.com>
2025-04-07 20:58:04 -0700

    FORMATTINGclear

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import { GoArrowRight } from "react-icons/go";
 const Button = ({ message }: { message: string }) => {
   return (
     <button className="bg-naama-blue-200 h-10 w-33 rounded-full">
-      { message }
+      {message}
       <GoArrowRight />
     </button>
   );
