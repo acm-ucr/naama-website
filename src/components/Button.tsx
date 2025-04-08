a54e8af23ad3449504454b813d2b64ddfc7b2df9
JSimjee <jameelsimjee@gmail.com>
2025-04-07 20:55:43 -0700

    maybe it's done?

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,11 @@
 import { GoArrowRight } from "react-icons/go";
 const Button = ({ message }: { message: string }) => {
-  return <button className="bg-naama-blue-200 h-10 w-33 rounded-full"></button>;
+  return (
+    <button className="bg-naama-blue-200 h-10 w-33 rounded-full">
+      message
+      <GoArrowRight />
+    </button>
+  );
 };
 // Font: Playfair Display, 18px, 96x22px, color #25459A
 // Button is 133x40 px, radius 25px
