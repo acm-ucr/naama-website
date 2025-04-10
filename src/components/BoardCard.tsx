bd452fe8264c355e2e4445177b954bd923f651ff
JSimjee <jameelsimjee@gmail.com>
2025-04-09 22:43:42 -0700

    started

---
+++ src/components/BoardCard.tsx
@@ -0,0 +1,15 @@
+const BoardCard = ({
+  name,
+  title,
+  yearMajor,
+  description,
+}: {
+  name: string;
+  title: string;
+  yearMajor: string;
+  description: string;
+}) => {
+  return {};
+};
+
+export default BoardCard;
