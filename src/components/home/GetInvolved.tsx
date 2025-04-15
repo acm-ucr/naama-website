cc750b76c4b88321b39cfb62068451597a212c83
hyacinth237 <acast494@ucr.edu>
2025-04-15 15:14:50 -0700

    Updated spacing between lines

--- src/components/home/GetInvolved.tsx
+++ src/components/home/GetInvolved.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const GetInvolved = () => {
   return (
     <div className="border-t-naama-blue-100 border-b-naama-blue-300 from-naama-blue-100 to bg-naama-ivory-100 flex h-4/4 w-4/4 flex-col items-center justify-center gap-4 border-t-10 border-b-20 bg-gradient-to-bl">
-      <div className="flex flex-col items-center text-center gap-2">
+      <div className="flex flex-col items-center gap-2 text-center">
         <div className="text-naama-blue-200 font-nunito text-3xl">
           Excited to
           <span className="italic"> Get Involved?</span>
