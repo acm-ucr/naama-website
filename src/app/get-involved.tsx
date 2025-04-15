0b8f01397561e4a735cf5f1b8b1e9ede1885465f
hyacinth237 <acast494@ucr.edu>
2025-04-15 14:29:37 -0700

    get involved

---
+++ src/app/get-involved.tsx
@@ -0,0 +1,22 @@
+// import { GoArrowRight } from "react-icons/go";
+
+const GetInvolved = () => {
+  return (
+    <div className="border-t-naama-blue-100 border-b-naama-blue-300 from-naama-blue-100 to bg-naama-ivory-100 flex h-4/4 w-4/4 flex-col items-center justify-center border-t-10 border-b-20 bg-gradient-to-bl">
+      <div className="flex flex-col items-center text-center">
+        <div className="text-naama-blue-200 font-nunito text-3xl">
+          Excited to
+          <span className="italic"> Get Involved?</span>
+        </div>
+        <div className="flex">
+          <div className="text-naama-blue-200 font-playfair">
+            click here for calendar 
+          </div>
+          {/* <GoArrowRight/> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GetInvolved;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import GetInvolved from "./get-involved";
+
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      Hello World
+      <GetInvolved />
     </div>
   );
 };
