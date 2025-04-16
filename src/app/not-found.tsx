48eb0ef64db9735a00635b2d7190d30f36dc4f05
Muhammad Sabeel <muhammadsabeel4@gmail.com>
2025-04-15 17:50:48 -0700

    Made text bigger

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ const NotFound = () => {
   return (
     <div className="bg-naama-blue-200 font-playfair flex h-screen flex-col items-center justify-center text-2xl text-white">
       <p className="text-6xl">404</p>
-      <p className="text-center text-2xl">
+      <p className="text-center text-4xl">
         Oh no! The page you're looking for can <br />
         not be found
       </p>
