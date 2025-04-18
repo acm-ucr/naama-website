beb309198048c81653e9c40b20d687a7a0c9ffb4
Mostafa Elmallah <mmmelmallah@gmail.com>
2025-04-19 01:45:17 +0200

    did it for board and gallery still couldn't find events

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,7 +1,7 @@
 const Board = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      Meet the Board
+    <div className="flex h-screen w-screen items-center justify-center bg-blue-800">
+      <h1 className="font-serif text-5xl text-white">Meet the Board</h1>
     </div>
   );
 };
--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,7 +1,7 @@
 const Gallery = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      Moments with NAAMA UCR
+    <div className="flex h-screen w-screen items-center justify-center bg-blue-800">
+      <h1 className="font-serif text-5xl text-white">Moments with NAAMA UCR</h1>
     </div>
   );
 };
