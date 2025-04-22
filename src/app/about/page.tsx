793875295e34b1d134f50aa7d98abc7ed701cfeb
hyacinth237 <acast494@ucr.edu>
2025-04-22 01:13:19 -0700

    Calls getinvolved in the about page and uses absolute pathing

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,9 @@
+import GetInvolved from "@/components/home/GetInvolved";
 const About = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       About Us
+      <GetInvolved />
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import GetInvolved from "../components/home/GetInvolved";
 import Button from "@/components/Button";
 import Footer from "@/components/Footer";
 
@@ -6,7 +5,6 @@ const Home = () => {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
       <Button message="Join Now" />
-      <GetInvolved />
       <Footer />
     </div>
   );
