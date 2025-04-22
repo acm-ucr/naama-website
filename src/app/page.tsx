c1e827a6cb34dca8369a2a529e66b3449add1ccd
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2025-04-21 21:29:33 -0700

    feat: add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Example from "@/components/Example";
+import NavBar from "@/components/NavBar";
 import Button from "@/components/Button";
 import Footer from "@/components/Footer";
 
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center">
+    <div className="bg-naama-blue-200 flex w-screen flex-col items-center justify-center">
+      <NavBar />
       <Example text1="Adjust" text2="these" text3="Props" />
       <Button message="Join Now" />
       <Footer />
