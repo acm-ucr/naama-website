7eee19a57dc2c994c27022ede059d05a56100ea1
Muhammad Sabeel <muhammadsabeel4@gmail.com>
2025-04-23 16:26:55 -0700

    Added come join us part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import NavBar from "@/components/NavBar";
 import Button from "@/components/Button";
 import Footer from "@/components/Footer";
+import JoinUs from "@/components/home/JoinUs";
 
 const Home = () => {
   return (
     <div className="bg-naama-blue-200 flex w-screen flex-col items-center justify-center">
       <NavBar />
       <Button message="Join Now" />
+      <JoinUs />
       <Footer />
     </div>
   );
