de621e571a04cd040fa7d9dbe1e577a120bbd46c
hyacinth237 <acast494@ucr.edu>
2025-04-23 09:46:26 -0700

    Fixed some text formatting

--- src/components/home/ExploreOurEvents.tsx
+++ src/components/home/ExploreOurEvents.tsx
@@ -1,11 +1,11 @@
 const ExploreOurEvents = () => {
   return (
-    <div className="bg-naama-blue-100 flex w-screen justify-center gap-5">
-      <div className="font-playfair flex-row pl-5 text-white">
+    <div className="bg-naama-blue-100 font-nunito flex w-screen justify-center gap-5 text-white">
+      <div className="font-playfair mr-5 w-1/3 items-center justify-center pl-5 text-2xl text-white">
         Explore <span className="italic"> Our Events </span>
       </div>
-      <div className="font-nunito flex flex-col pr-5 font-bold text-white">
-        <div className="">
+      <div className="flex w-2/3 flex-col pr-5 font-bold text-white">
+        <div className="row-col-2">
           <div className="flex font-bold">Workshops</div>
           <div className="flex text-xs">
             Our hands-on workshops provide members with practical skills and
