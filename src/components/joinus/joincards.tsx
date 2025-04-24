56839c3c8f3eb870150dbf9f5663f847eed0a8b1
Matthew Martinez <matthewmartinez@Mac.home>
2025-04-24 05:33:21 -0700

    fixed format

--- src/components/joinus/joincards.tsx
+++ src/components/joinus/joincards.tsx
@@ -7,7 +7,7 @@ interface JoinUsCardProps {
   altText?: string;
 }
 
-const JoinUsCard = ({ title, description, icon: Icon}: JoinUsCardProps) => {
+const JoinUsCard = ({ title, description, icon: Icon }: JoinUsCardProps) => {
   return (
     <div className="flex flex-wrap justify-center gap-8">
       <div className="bg-naama-ivory-100 shadow-3xl flex w-3/4 flex-col items-center rounded-3xl p-5 text-wrap">
@@ -17,8 +17,8 @@ const JoinUsCard = ({ title, description, icon: Icon}: JoinUsCardProps) => {
         <p className="text-naama-blue-200 font-sans-nunito mt-3 text-center">
           {description}
         </p>
-        <div className="bg-naama-blue-100 text-naama-blue-300 h-20 w-20 rounded-full flex items-center justify-center self-end">
-          <Icon className= "h-14 w-14"/>
+        <div className="bg-naama-blue-100 text-naama-blue-300 flex h-20 w-20 items-center justify-center self-end rounded-full">
+          <Icon className="h-14 w-14" />
         </div>
       </div>
     </div>
