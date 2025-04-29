206067b180ef1c2d47c4e15eaebe5ddbf3a1220a
Rachel  Moussa <rachelmoussa@Rachels-MacBook-Pro.local>
2025-04-29 14:32:02 -0700

    About Us Page merge conflict resolved

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,11 @@
+import AboutUs from "@/components/about/About";
 import GetInvolved from "@/components/about/GetInvolved";
 import ExploreOurEvents from "@/components/home/ExploreOurEvents";
 
 const About = () => {
   return (
     <div className="flex w-full flex-grow flex-col">
+      <AboutUs />
       <ExploreOurEvents />
       <GetInvolved />
     </div>
