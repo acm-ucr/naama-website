a3f05f1b3b04d75f092f529639b6572833e547cd
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2025-04-29 13:05:43 -0700

    feat: add className support for button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,19 @@
 import { GoArrowRight } from "react-icons/go";
 import Link from "next/link";
 
-const Button = ({ message, href }: { message: string; href: string }) => {
+const Button = ({
+  message,
+  href,
+  className,
+}: {
+  message: string;
+  href: string;
+  className?: string;
+}) => {
   return (
     <Link
       href={href}
-      className="font-playfair text-naama-blue-200 flex size-4 h-10 w-33 items-center justify-center gap-1.5 rounded-full bg-white tracking-tight drop-shadow-lg"
+      className={`font-playfair text-naama-blue-200 flex size-4 h-10 w-33 items-center justify-center gap-1.5 rounded-full bg-white tracking-tight drop-shadow-lg ${className || ""}`}
     >
       {message}
       <GoArrowRight />
