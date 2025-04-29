0ab0e2bf99a955f05da5567855b7236cf3ee098d
Muhammad Sabeel <muhammadsabeel4@gmail.com>
2025-04-29 14:52:47 -0700

    Removed bold

--- src/components/home/JoinUs.tsx
+++ src/components/home/JoinUs.tsx
@@ -2,10 +2,7 @@ const JoinUs = () => {
   return (
     <div className="bg-naama-blue-200 flex h-screen flex-col items-center justify-center text-center text-white">
       <div className="font-playfair text-5xl">
-        Come{" "}
-        <span className="italic">
-          <span className="font-bold">Join</span> Us
-        </span>
+        Come <span className="italic"> Join Us</span>
       </div>
       <p className="py-15 text-2xl">
         Whether you're looking to expand your knowledge or gain hands-on <br />
