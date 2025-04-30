f7ab1f20c5b8074ff6ebfb5319f4bd81eb7141bd
Muhammad Sabeel <muhammadsabeel4@gmail.com>
2025-04-30 16:30:56 -0700

    Edited page.tsx and comejoinus.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import NavBar from "@/components/NavBar";
 import Button from "@/components/Button";
-import Footer from "@/components/Footer";
 import JoinUs from "@/components/home/JoinUs";
 import ComeJoinUs from "@/components/home/ComeJoinUs";
 import Feed from "@/components/home/Feed";
@@ -10,14 +8,12 @@ import WhoAreWe from "@/components/home/WhoAreWe";
 const Home = () => {
   return (
     <div className="bg-naama-blue-200 flex w-screen flex-col items-center justify-center">
-      <NavBar />
       <Hero />
       <WhoAreWe />
       <ComeJoinUs />
       <JoinUs />
       <Button message="Join Now" href="error" />
       <Feed />
-      {/* <Footer /> */}
     </div>
   );
 };
