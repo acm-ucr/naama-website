527f2a13ec0f0dd5d6e5f47f4e5c359fe46c1600
Matthew Martinez <matthewmartinez@ucr-secure-25-10-13-151-220.wnet.ucr.edu>
2025-04-30 16:35:23 -0700

    started issue

--- src/components/home/WhoAreWe.tsx
+++ src/components/home/WhoAreWe.tsx
@@ -1,5 +1,37 @@
+import Image from "next/image";
+import groupHome from "@/public/home/groupHome.webp";
+
 const WhoAreWe = () => {
-  return <div>Who Are We?</div>;
+  return (
+    <div className="relative h-[500px] w-full">
+      <Image
+        src={groupHome}
+        alt="groupHome"
+        className="absolute h-full w-full opacity-80"
+        priority
+      />
+      <div className="bg-naama-ivory-100 absolute inset-0 opacity-80" />
+
+      <div className="relative z-10 flex items-center justify-center">
+        <h1 className="text-naama-blue-300 font-playfair text-6xl">
+          <span className="underline">Who</span>
+          are
+          <span className="italic"> We</span>?
+        </h1>
+        <p className="font-nunito text-naama-blue-300">
+          NAAMA NextGen UCR is a student chapter of the National Arab American
+          Medical Association dedicated to
+          <span className="font-extrabold"> empowering </span>
+          Arab American students pursuing careers in healthcare. We strengthen
+          professional growth, cultural awareness, and community engagement
+          through
+          <span className="font-extrabold"> mentorship</span>,
+          <span className="font-extrabold"> networking</span>, and
+          <span className="font-extrabold"> service initiatives</span>.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default WhoAreWe;
