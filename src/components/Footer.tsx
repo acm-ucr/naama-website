8019d31fcf0190cca30a308ab45178a23824f208
Gideon Cameron <gideonwork20@gmail.com>
2025-05-02 04:31:52 -0700

    Fix: Updated footer links to correct routes and external URLs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import logo from "@/../public/logo.svg";
+import logo from "@/public/logo.svg";
 
 const Footer = () => {
   return (
@@ -25,23 +25,23 @@ const Footer = () => {
         <div className="flex justify-center gap-4">
           <p className="font-bold">UCR Chapter</p>
           <div className="flex flex-col gap-2">
-            <Link href="/">Home</Link>
-            <Link href="/">About Us</Link>
-            <Link href="/">Contact Info</Link>
+          <Link href="/">Home</Link>
+            <Link href="/about">About Us</Link>
+            <Link href="/">Contact Info</Link> 
           </div>
         </div>
         <div className="flex justify-center gap-4">
           <p className="font-bold">Important Links</p>
           <div className="flex flex-col gap-2">
-            <Link href="/">Membership Form</Link>
-            <Link href="/">Event Calendar</Link>
+          <a href="https://naamanextgen.com/" target="_blank" rel="noopener noreferrer">Membership Form</a>
+          <Link href="/calendar">Event Calendar</Link>
           </div>
         </div>
         <div className="flex justify-center gap-4">
           <p className="font-bold">Social Media</p>
           <div className="flex flex-col gap-2">
-            <Link href="/">Instagram</Link>
-            <Link href="/">Linktree</Link>
+          <a href="https://www.instagram.com/naamaucr" target="_blank" rel="noopener noreferrer">Instagram</a>
+          <a href="https://linktr.ee/naamaucr" target="_blank" rel="noopener noreferrer">Linktree</a>
           </div>
         </div>
       </div>
