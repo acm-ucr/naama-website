f72ae78f10f1ba25b3ea5a8fbcecebccea55efc5
Gideon Cameron <gideonwork20@gmail.com>
2025-05-02 04:33:42 -0700

    chore: run Prettier to pass pre-push checks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -25,23 +25,41 @@ const Footer = () => {
         <div className="flex justify-center gap-4">
           <p className="font-bold">UCR Chapter</p>
           <div className="flex flex-col gap-2">
-          <Link href="/">Home</Link>
+            <Link href="/">Home</Link>
             <Link href="/about">About Us</Link>
-            <Link href="/">Contact Info</Link> 
+            <Link href="/">Contact Info</Link>
           </div>
         </div>
         <div className="flex justify-center gap-4">
           <p className="font-bold">Important Links</p>
           <div className="flex flex-col gap-2">
-          <a href="https://naamanextgen.com/" target="_blank" rel="noopener noreferrer">Membership Form</a>
-          <Link href="/calendar">Event Calendar</Link>
+            <a
+              href="https://naamanextgen.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Membership Form
+            </a>
+            <Link href="/calendar">Event Calendar</Link>
           </div>
         </div>
         <div className="flex justify-center gap-4">
           <p className="font-bold">Social Media</p>
           <div className="flex flex-col gap-2">
-          <a href="https://www.instagram.com/naamaucr" target="_blank" rel="noopener noreferrer">Instagram</a>
-          <a href="https://linktr.ee/naamaucr" target="_blank" rel="noopener noreferrer">Linktree</a>
+            <a
+              href="https://www.instagram.com/naamaucr"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Instagram
+            </a>
+            <a
+              href="https://linktr.ee/naamaucr"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Linktree
+            </a>
           </div>
         </div>
       </div>
