26cefaa82916d8d3c5574e85d40bcb6b94050b8b
Matthew Martinez <matthewmartinez@ucr-secure-25-10-13-151-51.wnet.ucr.edu>
2025-05-06 14:43:06 -0700

    finished component. header and paragraph could be more spaced out

--- src/components/home/WhoAreWe.tsx
+++ src/components/home/WhoAreWe.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import groupHome from "@/public/home/groupHome.webp";
+import Button from "@/components/Button";
 
 const WhoAreWe = () => {
   return (
@@ -12,13 +13,12 @@ const WhoAreWe = () => {
       />
       <div className="bg-naama-ivory-100 absolute inset-0 opacity-80" />
 
-      <div className="relative z-10 flex items-center justify-center">
+      <div className="relative z-10 flex h-full w-full items-center justify-center gap-20">
         <h1 className="text-naama-blue-300 font-playfair text-6xl">
-          <span className="underline">Who</span>
-          are
+          <span className="underline">Who</span> are
           <span className="italic"> We</span>?
         </h1>
-        <p className="font-nunito text-naama-blue-300">
+        <p className="font-nunito text-naama-blue-300 mt-5 max-w-3xl">
           NAAMA NextGen UCR is a student chapter of the National Arab American
           Medical Association dedicated to
           <span className="font-extrabold"> empowering </span>
@@ -29,6 +29,7 @@ const WhoAreWe = () => {
           <span className="font-extrabold"> networking</span>, and
           <span className="font-extrabold"> service initiatives</span>.
         </p>
+        <Button className="mb-10 self-end" message="Learn More" href="/" />
       </div>
     </div>
   );
