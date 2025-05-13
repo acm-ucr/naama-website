fd1f3582ec99092076690629f880a6dd08ac51ae
hyacinth237 <acast494@ucr.edu>
2025-05-13 14:27:33 -0700

    Formatting

--- src/components/home/JoinCard.tsx
+++ src/components/home/JoinCard.tsx
@@ -10,14 +10,14 @@ interface JoinUsCardProps {
 const JoinUsCard = ({ title, description, icon: Icon }: JoinUsCardProps) => {
   return (
     <div className="flex justify-center px-4 sm:px-0">
-      <div className="bg-naama-ivory-100 flex w-full md:w-3/4 flex-col items-center rounded-4xl p-6 sm:p-8 text-wrap shadow-md shadow-black">
-        <div className="text-naama-blue-200 font-nunito text-lg sm:text-xl font-bold">
+      <div className="bg-naama-ivory-100 flex w-full flex-col items-center rounded-4xl p-6 text-wrap shadow-md shadow-black sm:p-8 md:w-3/4">
+        <div className="text-naama-blue-200 font-nunito text-lg font-bold sm:text-xl">
           {title}
         </div>
         <p className="text-naama-blue-200 font-sans-nunito mt-3 text-center">
           {description}
         </p>
-        <div className="bg-naama-blue-100/60 text-naama-blue-300 self-center md:self-end rounded-full p-2">
+        <div className="bg-naama-blue-100/60 text-naama-blue-300 self-center rounded-full p-2 md:self-end">
           <Icon className="h-10 w-10 stroke-1" />
         </div>
       </div>
