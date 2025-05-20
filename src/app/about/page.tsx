11d9fce827e85b8e6aff4a3e363ab1acd9cb3b64
JSimjee <jameelsimjee@gmail.com>
2025-05-19 21:00:39 -0700

    made changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,15 @@
+import Image from "next/image";
 import AboutUs from "@/components/about/About";
 import GetInvolved from "@/components/about/GetInvolved";
 import ExploreOurEvents from "@/components/about/ExploreOurEvents";
-
+import aboutGroup from "../../../public/about/groupAbout.webp";
 const About = () => {
   return (
     <div className="flex w-full flex-grow flex-col">
-      <AboutUs />
+      <div className="mx-12 flex columns-2 justify-center">
+        <AboutUs />
+        <Image src={aboutGroup} alt={"About Group"} className="mx-12 mb-18" />
+      </div>
       <ExploreOurEvents />
       <GetInvolved />
     </div>
