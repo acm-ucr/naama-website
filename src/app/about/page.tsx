11c8f871fe1355910f30e1b15c2bd4cea935058c
JSimjee <jameelsimjee@gmail.com>
2025-05-19 23:30:32 -0700

    fixed image stretching out

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,10 +6,22 @@ import aboutGroup from "../../../public/about/groupAbout.webp";
 const About = () => {
   return (
     <div className="flex w-full flex-grow flex-col">
+      {/* <div className="columns-2">
+        <div className="">
+          <AboutUs/>
+        </div>
+        <Image src={aboutGroup} alt={"About Group"} className="mx-12 mb-18" />
+      </div> */}
+
       <div className="mx-12 mt-12 flex columns-2 justify-center">
         <AboutUs />
-        <Image src={aboutGroup} alt={"About Group"} className="mx-12 mb-18" />
+        <Image
+          src={aboutGroup}
+          alt={"About Group"}
+          className="mx-12 mb-18 object-contain"
+        />
       </div>
+
       <ExploreOurEvents />
       <GetInvolved />
     </div>
