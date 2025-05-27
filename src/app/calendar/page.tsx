95f1f0805f6489a83a871e96f473cb19d769c77b
Rachel  Moussa <rachelmoussa@Rachels-MacBook-Pro.local>
2025-05-27 14:41:37 -0700

    Ran formatting

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -4,17 +4,18 @@ const Board = () => {
   return (
     <div className="flex w-full grow flex-col items-center justify-center pl-15">
       <Title title="Events Calendar" />
-      
+
       <p className="mt-10 flex w-screen pl-50 text-white">
-        Stay up to date with the latest events! Whether you're
-        looking to connect with like-minded 
+        Stay up to date with the latest events! Whether you're looking to
+        connect with like-minded
       </p>
-      
-      <p className="flex w-screen pl-50 text-white">        
-        peers or gain insights into the medical field, there's something for everyone. Don't miss out 
+
+      <p className="flex w-screen pl-50 text-white">
+        peers or gain insights into the medical field, there's something for
+        everyone. Don't miss out
       </p>
-      
-      <p className="flex w-screen pl-50 text-white mb-25">        
+
+      <p className="mb-25 flex w-screen pl-50 text-white">
         and check out the upcoming events today!
       </p>
     </div>
