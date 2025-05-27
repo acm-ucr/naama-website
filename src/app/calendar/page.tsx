bdf35cffd465e32cb2a650f4873720057f640735
Rachel  Moussa <rachelmoussa@Rachels-MacBook-Pro.local>
2025-05-27 14:59:00 -0700

    fixed paragrpah breaks

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -2,21 +2,14 @@ import Title from "@/components/Title";
 
 const Board = () => {
   return (
-    <div className="flex w-full grow flex-col items-center justify-center pl-15">
+    <div className="flex w-full grow flex-col justify-center pl-60">
       <Title title="Events Calendar" />
 
-      <p className="mt-10 flex w-screen pl-50 text-white">
+      <p className="mt-10 mb-25 flex w-1/2 pr-20 text-white">
         Stay up to date with the latest events! Whether you're looking to
-        connect with like-minded
-      </p>
-
-      <p className="flex w-screen pl-50 text-white">
-        peers or gain insights into the medical field, there's something for
-        everyone. Don't miss out
-      </p>
-
-      <p className="mb-25 flex w-screen pl-50 text-white">
-        and check out the upcoming events today!
+        connect with like-minded peers or gain insights into the medical field,
+        there's something for everyone. Don't miss out and check out the
+        upcoming events today!
       </p>
     </div>
   );
