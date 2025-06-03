443f9b9cb5b4b4bd568189e4dec4049df365556b
arieen <arielleharyanto@gmail.com>
2025-06-02 17:27:20 -0700

    fixing build

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,5 +1,6 @@
 import CalendarEvents from "@/components/calendar/CalendarEvents";
 import EventsDescription from "@/components/calendar/EventsDescription";
+import Title from "@/components/Title";
 
 const Calendar = () => {
   return (
