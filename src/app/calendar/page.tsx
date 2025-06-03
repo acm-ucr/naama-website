00e1f87b74bda2ef58227b46c37b5e8760108e10
arieen <arielleharyanto@gmail.com>
2025-06-02 17:11:44 -0700

    code quality round 1

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,17 +1,12 @@
 import Title from "@/components/Title";
+import Description from "@/components/calendar/EventDescription";
 
 const Board = () => {
   return (
-    <div className="flex w-full grow flex-col justify-center pl-60">
+    <>
       <Title title="Events Calendar" />
-
-      <p className="mt-10 mb-25 flex w-1/2 pr-20 text-white">
-        Stay up to date with the latest events! Whether you're looking to
-        connect with like-minded peers or gain insights into the medical field,
-        there's something for everyone. Don't miss out and check out the
-        upcoming events today!
-      </p>
-    </div>
+      <Description />
+    </>
   );
 };
 
