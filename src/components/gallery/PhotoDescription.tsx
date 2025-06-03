0242fac1bb8c8c885831955e5ed9efedacab4997
Rachel  Moussa <rachelmoussa@Rachels-MacBook-Pro.local>
2025-06-03 14:47:49 -0700

    Mobile moments

--- src/components/gallery/PhotoDescription.tsx
+++ src/components/gallery/PhotoDescription.tsx
@@ -1,7 +1,9 @@
 const Description = () => {
   return (
-    <p className="my-10 flex md:w-1/2 md:pl-45 text-center md:px-0 px-20 md:text-left text-pretty text-white">
-      Step into our gallery and relive the highlights of our journey in medicine! These snapshots capture the dedication, collaboration, and passion that define our community.
+    <p className="my-10 flex px-20 text-center text-pretty text-white md:w-1/2 md:px-0 md:pl-45 md:text-left">
+      Step into our gallery and relive the highlights of our journey in
+      medicine! These snapshots capture the dedication, collaboration, and
+      passion that define our community.
     </p>
   );
 };
