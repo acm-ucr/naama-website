0e6ea676df195c668bc1892e513f699b50907e17
Brandon Tran <brandontran1220@gmail.com>
2025-06-04 14:58:24 -0700

    Fixed height on join community

--- src/components/gallery/PhotoDescription.tsx
+++ src/components/gallery/PhotoDescription.tsx
@@ -1,6 +1,6 @@
 const Description = () => {
   return (
-    <p className="my-10 flex w-1/2 pl-55 text-pretty text-white">
+    <p className="mx-auto my-10 flex w-3/4 text-center text-pretty text-white md:text-left">
       Step into our gallery and relive the highlights of our journey in
       medicine! These snapshots capture the dedication, collaboration, and
       passion that define our community.
