9d8ba6484dcc89f375b603831c8829c0bf90ea96
Mostafa Elmallah <mmmelmallah@gmail.com>
2025-06-04 15:58:00 -0700

    fixed formatting an dother issues

--- src/components/home/WhoAreWe.tsx
+++ src/components/home/WhoAreWe.tsx
@@ -13,7 +13,7 @@ const WhoAreWe = () => {
       <div className="bg-naama-ivory-100 absolute inset-0 opacity-80" />
 
       <div className="relative z-10 flex h-full w-full flex-col items-center justify-center gap-20 text-center md:flex md:px-32">
-        <h1 className="text-naama-blue-200 font-playfair text-l font-bold whitespace-nowrap">
+        <p className="text-naama-blue-200 font-playfair text-l font-bold whitespace-nowrap">
           <span className="underline decoration-2 underline-offset-8">Who</span>{" "}
           are
           <span className="italic"> We</span>?
