3689139c9768f66519c1df4b6c11b0d5335e5461
arieen <arielleharyanto@gmail.com>
2025-06-05 19:29:39 -0700

    touch up

--- src/components/home/WhoAreWe.tsx
+++ src/components/home/WhoAreWe.tsx
@@ -4,22 +4,21 @@ import Button from "@/components/Button";
 
 const WhoAreWe = () => {
   return (
-    <div className="relative h-96 w-full">
+    <div className="relative h-[60vh] w-full md:h-[60vh]">
       <Image
         src={groupHome}
         alt="groupHome"
         className="absolute h-full w-full object-cover opacity-80"
       />
       <div className="bg-naama-ivory-100 absolute inset-0 opacity-80" />
-      <div className="relative z-10 flex h-full w-full flex-col items-center justify-center gap-20 text-center md:flex md:px-32">
-        <div className="h-16"></div>
-        <p className="text-naama-blue-200 font-playfair text-l font-bold whitespace-nowrap">
+      <div className="relative flex h-full w-full flex-col items-center justify-center px-5 md:flex md:flex-row md:gap-20 lg:px-20">
+        <p className="text-naama-blue-200 font-playfair pt-10 text-3xl font-bold whitespace-nowrap md:pt-0 md:text-5xl">
           <span className="underline decoration-2 underline-offset-8">Who</span>{" "}
           are
           <span className="italic"> We</span>?
         </p>
-        <div className="flex flex-col gap-4">
-          <p className="font-nunito text-naama-blue-200 p-10 text-xl">
+        <div className="flex flex-col">
+          <p className="font-nunito text-naama-blue-200 text-md py-5 text-center md:py-10 md:text-left md:text-xl">
             NAAMA NextGen UCR is a student chapter of the National Arab American
             Medical Association dedicated to
             <span className="font-extrabold"> empowering </span>
@@ -31,7 +30,7 @@ const WhoAreWe = () => {
             <span className="font-extrabold"> service initiatives</span>.
           </p>
           <Button
-            className="mx-auto -mt-4"
+            className="mx-auto md:mr-14 md:ml-auto"
             message="Learn More"
             href="/about"
           />
