120c0ae6d89727feb8e3f51394aefd40bf4720d0
hyacinth237 <acast494@ucr.edu>
2025-06-14 16:39:36 -0700

    Updated formatting

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -7,17 +7,17 @@ import Description from "@/components/gallery/PhotoDescription";
 import { motion } from "motion/react";
 
 const titleAnimation = {
-  initial: {opacity: 0,x: -50},
-  whileInView: {opacity: 1,x: 0},
-  viewport: {once:true},
-  transition: {duration: 0.9, ease: "easeOut"},
+  initial: { opacity: 0, x: -50 },
+  whileInView: { opacity: 1, x: 0 },
+  viewport: { once: true },
+  transition: { duration: 0.9, ease: "easeOut" },
 };
 
 const textAnimation = {
-  initial: {opacity: 0,x: -50},
-  whileInView: {opacity: 1,x: 0},
-  viewport: {once:true},
-  transition: {duration: 0.9, ease: "easeOut", delay: 0.3},
+  initial: { opacity: 0, x: -50 },
+  whileInView: { opacity: 1, x: 0 },
+  viewport: { once: true },
+  transition: { duration: 0.9, ease: "easeOut", delay: 0.3 },
 };
 
 const Board = () => {
@@ -26,8 +26,8 @@ const Board = () => {
       <motion.div {...titleAnimation}>
         <Title title="Moments with NAAMA UCR" />
       </motion.div>
-        <motion.div {...textAnimation}>
-      <Description />
+      <motion.div {...textAnimation}>
+        <Description />
       </motion.div>
       <PictureGallery />
       <JoinCommunity />
