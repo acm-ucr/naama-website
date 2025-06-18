3e4d3e2f600b4f53139cd45b43ffd23d89674931
unknown <twu148@ucr.edu>
2025-06-18 11:48:26 -0700

    Come join us responsiveness

--- src/components/home/ComeJoinUs.tsx
+++ src/components/home/ComeJoinUs.tsx
@@ -19,9 +19,9 @@ const ComeJoinUs = () => {
       <h3 className="font-playfair text-5xl">
         Come <span className="italic"> Join Us</span>
       </h3>
-      <p className="w-full items-center justify-center py-15 text-center text-2xl">
-        Whether you're looking to expand your knowledge or gain hands-on <br />
-        experience, we are here to help you pave the way to <br />{" "}
+      <p className="items-center justify-center px-12 w-2/3 py-12 text-center text-lg md:text-xl">
+        Whether you're looking to expand your knowledge or gain hands-on
+        experience, we are here to help you pave the way to {" "}
         <span className="font-bold">a successful future in medicine.</span>
       </p>
       <JoinUs />
--- src/components/home/JoinCard.tsx
+++ src/components/home/JoinCard.tsx
@@ -9,17 +9,15 @@ interface JoinUsCardProps {
 
 const JoinUsCard = ({ title, description, icon: Icon }: JoinUsCardProps) => {
   return (
-    <div className="flex w-full justify-center">
-      <div className="bg-naama-ivory-100 flex w-full flex-col items-center rounded-4xl p-6 text-wrap shadow-md shadow-black sm:p-8">
-        <div className="text-naama-blue-200 font-nunito text-center text-lg font-bold sm:text-xl">
-          {title}
-        </div>
-        <p className="text-naama-blue-200 font-sans-nunito my-3 text-center">
-          {description}
-        </p>
-        <div className="bg-naama-blue-100/60 text-naama-blue-300 self-center rounded-full p-2 md:self-end">
-          <Icon className="h-10 w-10 stroke-1" />
-        </div>
+    <div className="bg-naama-ivory-100 justify-center flex w-full flex-col items-center rounded-4xl p-6 text-wrap shadow-md shadow-black sm:p-8">
+      <h3 className="text-naama-blue-200 font-nunito text-center text-lg font-bold sm:text-xl">
+        {title}
+      </h3>
+      <p className="text-naama-blue-200 font-sans-nunito my-3 text-center">
+        {description}
+      </p>
+      <div className="bg-naama-blue-100/60 text-naama-blue-300 self-center rounded-full p-2 md:self-end mt-auto">
+        <Icon className="h-10 w-10 stroke-1" />
       </div>
     </div>
   );
