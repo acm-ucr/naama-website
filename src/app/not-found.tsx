6c2fd5a9bc9a22279830237bf73325be381e7cdc
unknown <twu148@ucr.edu>
2025-06-18 12:32:03 -0700

    404 animation and mobile responsivness

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,28 @@
+"use client";
+
+import { motion } from "motion/react";
+
 import Button from "@/components/Button";
+
+const animationProps = {
+  initial: { opacity: 0, y: 40 },
+  whileInView: { opacity: 1, y: 0 },
+  transition: { duration: 0.9, ease: "easeOut" },
+  viewport: { once: true },
+};
 
 const NotFound = () => {
   return (
-    <div className="font-playfair flex min-h-screen grow flex-col items-center justify-center gap-4 text-white">
-      <p className="mt-4 mb-2 text-5xl sm:mt-2 sm:text-6xl">404</p>
-      <p className="w-2/3 text-center text-4xl sm:w-1/3">
+    <motion.div
+      {...animationProps}
+      className="font-playfair flex min-h-[75dvh] grow flex-col items-center justify-center gap-4 text-white"
+    >
+      <h1 className="mt-2 mb-2 text-5xl md:mt-4 md:text-6xl">404</h1>
+      <p className="w-1/3 text-center text-xl/relaxed md:text-4xl/relaxed">
         Oh no! The page you're looking for can't be found!
       </p>
-      <Button message="Go Back Home" href="/" className="mt-4" />
-    </div>
+      <Button message="Return Home" href="/" className="mt-4" />
+    </motion.div>
   );
 };
 
